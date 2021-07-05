6c0a60043f6fdc60e142b15d1ebc623078277f02
AbantikaKarmakar <85815714+AbantikaKarmakar@users.noreply.github.com>
2021-07-05 21:24:40 +0530

    Initial Commit

---
+++ emailextractor.py
@@ -0,0 +1,205 @@
+import re
+
+mystr = '''
+Zoho.com
+BiginCampaignsRecruitProjectsTeamInboxTransMailAll Products
+Search for product overviews, FAQs, and more...
+SIGN INGET STARTED
+ZOHOMailGET STARTED
+Features
+Pricing
+Enterprise
+Resources
+Partner with us
+Contact Us
+Mail logoMail
+Features
+Pricing
+Enterprise
+Resources
+Partner with us
+Contact Us
+	
+Zoho Mail
+Tips and Tricks
+
+Learn how to use Zoho Mail efficiently and with ease.
+
+SIGN UP FOR FREE
+Search 
+
+
+CATEGORIES
+Getting Started
+Setting up Zoho Mail
+Sending emails
+How to choose a professional email address for your users
+What is a Professional email address?
+With everything happening online and in the cloud, each of us has our own email account and use our email address for all our communications. Moreover, our email address has become our online identity and a base for all the personal and work related services that we use. Hence it is essential that we have different email accounts or at the minimum, different email addresses for personal and professional communication purposes. Our work related email address should be used only for business communications and for professional relationships. 
+
+A business email address is the one you mainly use to communicate with your business associates, your colleagues, your customers and your partners. Only if the business email address includes the custom domain you have registered for your business, it will be a professional email address. A professional email address is the email address, used for business communications that is based on your custom domain. In other words, a professional email address is one that has your business name in it, in the form of a domain name. 
+
+What are the benefits of a professional email address? 
+When you create a personal email address from popular email service providers with a generic domain, you can even pick a fancy name or a funny acronym for your email address. However, when it involves business communications, your email address and your employees' email address should not be casual. Along with professional email addresses, following email etiquette rules helps in projecting your professionalism and promoting your brand.
+
+Your email address should be based on your domain and also have a professional touch. A professional email address provides you with the following benefits: 
+
+Branding 
+Builds trust 
+Authenticity 
+Reliability 
+Ease of recalling
+Better deliverability and reputation
+How to choose a professional email address? 
+In order to maintain consistency while creating the email accounts, and to ensure that every employee has a professional email address, you need to plan and determine the format in which you want the email addresses of users. 
+
+Professional email address 
+
+Some of the most popular and commonly used formats of email addresses are as follows: 
+
+firstname.secondname@domain.com
+Example: peter.parker@zylker.com
+
+This is one of the most commonly used email formats around the globe. This type of email address sounds professional as it involves both first name and last name. In an organization, even if there are multiple people with same first names, the possibility of the same last name is much less and hence makes the user and the email address connect less ambiguous. This format is suitable for organizations of all sizes. Even in case of startups or small businesses, this will help in long term planning and helps in avoiding conflicts if the organization grows many fold.  
+
+firstname.initial@domain.com
+Example: peter.p@zylker.com 
+
+This is another popular format that is being used by organizations across the world. This email address format is preferred because it is personal and easily rememberable - since it is short and involves the first name of the user. However there might be conflicts when people have last names beginning with the same initials. Most admins prefer this format and use shortened form of the second name or two letters from the second name to avoid conflicts. 
+
+Example: peter.pk@zylker.com,
+
+peter.pa@zylker.com when there are two more persons called Peter Parker in the same organization. 
+
+firstname@domain.com 
+Example: peter@zylker.com 
+
+The customers and the business contacts will be able to easily identify the person who sent the email and connect with them. This is ideal and highly suitable for startups and small businesses with 10 users or less.  However, as the business/ organization grows bigger, there might be conflicts based on user names and this confusion gets even more complex, when both have customer-facing roles. It also causes confusion in internal emails when there are multiple people with the same first name. This format is highly popular only because the addresses are easy to remember.
+
+In case of bigger organizations, instead of assigning email address in this format to all users, email addresses in this format can be assigned as email aliases to prominent people in the organization. Having such an email address as an alias will help them have their primary email address private and hence easily differentiate internal and important emails from junk emails they receive.
+
+How to create professional email address for all the users using Zoho Mail? 
+To create a professional email address with your domain, you need to set up your domain with Zoho first. Follow the step by step process explained in this page to complete your domain setup. Once you finish verifying your domain with Zoho Mail, you have to add users to create their email accounts. In case you do not have a domain yet, you can buy a domain with Zoho. The domains registered via Zoho are already fully set up and can be used instantly. You can start adding users directly. 
+
+Zoho Mail supports multiple ways to create and choose professional email addresses for users using Zoho Mail.
+
+You can add the users one-by-one providing their first name, last name, and the email address to be created for them along with the password that should be used for them to login. This method is better and well preferred when you have to create 10 users or less.
+Adding users to organization 
+However even if there are 5+ user accounts to be created, it is recommended to use 'Import from CSV' to create uers from the details uploaded from a CSV file.
+You can use the 'Import' options available to import users from CRM  or other Zoho Services or directly import from GSuite.  Detailed instructions are available on this help page. 
+Group email addresses: Function/ Purpose-based professional email addresses
+Another important aspect that is often overlooked when creating an email address is the 'purpose' based email addresses. As the organization grows bigger, it is recommended to use generic or purpose based email addresses like contact@domain.com, support@domain.com, info@domain.com etc which suit the specific purposes. This will ensure that even if employees leave the organization, the email addresses for important purposes like contact@ will still be functional. In the initial stages of an organization, the responsibility of support, sales etc is at an individual level, and the communication happens from the customer or lead to the person directly. However to make the communication more standard and professional, it is better to ensure that such email addresses are generic group addresses which is managed by a team instead of individuals. 
+
+Common practice for group emails:
+To have a professional communication practice, it is recommended to have generic email accounts based on various function groups in the organization, based on their work nature. The following are some of the standard generic email accounts, that every organization uses for various organization functions.
+
+Public - External group email accounts: 
+External or Public email addresses are generally used to provide options for external contacts of the organizations to communicate with the different functions of the organization. When setting up these accounts, the admins should ensure that the group email accounts have permission to receive emails from external accounts. Further, the emails to the group should be properly channelized to the concerned team. 
+
+Examples: 
+
+support@domain.com
+sales@domain.com
+info@domain.com
+careers@domain.com
+enquiries@domain.com
+news@domain.com 
+Organization - Internal group email accounts: 
+Organization or Internal group email addresses are used for internal organization employees to reach internal groups and internal functions meant for employees. These accounts can be set up with moderation to receive emails only from the employees within the organization. 
+
+Examples: 
+
+payroll@domain.com
+hrqueries@domain.com
+systemadmin@domain.com
+employee-care@domain.com​ 
+How to create professional email address for all the generic accounts or distribution lists using Zoho Mail? 
+To create a professional email address with your domain for generic accounts or distribution lists for your domain, you need to create an email group with public access. Refer here for detailed instructions about creating and managing group email accounts with professional email addresses in Zoho Mail.
+
+The following are some of the most useful features when creating domain based group accounts in Zoho Mail: 
+
+You can add multiple members and assign multiple moderators for each group. 
+The moderator will have the group permissions and can further add users or change the settings of that group. The moderator can also moderate the emails sent to the group and approve or reject them. 
+The users can send emails using the group address. This feature can be extended by the administrator for the entire group members or only to specific members. 
+When users send emails using a group email address, their individual email address will not be exposed to the recipient. 
+The administrator can enable 'Streams' feature for the group using which the group members can collaborate and communicate within the group right from their Inbox, 
+ 
+
+SIGN UP FOR FREE
+   
+Mail online gate passGetting started with Zoho Mail
+Features
+Secure Email
+Commitment to Privacy
+Enterprise hosting
+Hassle free migration
+Email archival and EDiscovery
+Comparison guides
+Google Workspace
+Microsoft 365
+Rackspace
+GoDaddy
+HostGator, ResellerClub and BigRock
+Quick links
+Buy domain
+Partner with us
+Supported languages
+Community
+Admin Guide
+Explore
+What's New
+Set up domain with Zoho Mail
+Create a free business email
+Manage your inbox
+Search your inbox
+Deployment guides
+Complete setup guide
+Solopreneur
+Small business and startup
+Medium and large business
+Enterprise
+Follow us on
+FacebookTwitterYoutube
+Other Products by Zoho Mail
+TeamInbox
+
+Shared Mailbox
+Toolkit
+
+MX Lookup Tool
+TransMail
+
+Transactional email service
+Also a part of
+Workplace
+
+Email and Office Suite
+Choose Privacy. Choose Zoho.
+
+ 
+ 
+ 
+ 
+sales@zohocorp.com
+Search for product overviews, FAQs, and more...
+
+Zoho HomeContact UsSecurity IPR ComplaintsAnti-spam PolicyTerms of ServicePrivacy PolicyCookie PolicyGDPR ComplianceAbuse Policy
+© 2021, Zoho Corporation Pvt. Ltd. All Rights Reserved.
+
+  
+Next Steps
+Compare plans
+Request a quote
+Sign up for Free Trial
+'''
+list1 = re.findall(r'\w+@\S+\w', mystr)
+textfile = open("1.txt", "a")
+j=1
+for i in list1:
+    textfile.write(i + "\n")
+    j = j+1
+
+textfile.close()
+
+print(f"Email's are:{list1}")
+print(f"Total Email's are:{j-1}")
